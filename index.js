f1b5eeadbdf567964b2ab5628652800936c94b5f
Krishna Vamsi <krishnavamsigoli@gmail.com>
2025-04-21 19:14:04 +0530

    db env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
@@ -8,45 +9,50 @@ const port = 4003; // different from task & notification services
 app.use(cors());
 app.use(express.json());
 
-const db = new Pool({ user: 'postgres', password: 'Lovealll@7', host: 'localhost', port: 5432, database: 'taskdb' });
-
+const db = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: 'taskdb'
+});
 // Test route
 app.get('/', (req, res) => {
- res.send('Analytics Service is running');
+  res.send('Analytics Service is running');
 });
 
 app.get('/analytics/task-status', async (req, res) => {
- try {
-  const result = await db.query(`
+  try {
+    const result = await db.query(`
       SELECT status, COUNT(*) as count
       FROM tasks
       GROUP BY status
     `);
-  res.json(result.rows);
- } catch (err) {
-  console.error('Error fetching task status counts:', err);
-  res.status(500).send('Internal Server Error');
- }
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching task status counts:', err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // In analytics-service (Express server)
 app.get('/analytics/tasks-per-assignee', async (req, res) => {
- try {
-  const result = await db.query(`
+  try {
+    const result = await db.query(`
       SELECT assignee, COUNT(*) as count
       FROM tasks
       GROUP BY assignee
     `);
-  res.json(result.rows);
- } catch (err) {
-  console.error('Error fetching analytics:', err);
-  res.status(500).json({ error: 'Internal server error' });
- }
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching analytics:', err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 
 app.listen(port, () => {
- console.log(`Analytics Service running on http://localhost:${port}`);
+  console.log(`Analytics Service running on http://localhost:${port}`);
 });
 
 module.exports = { app, db };
