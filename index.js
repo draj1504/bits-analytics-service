8ec26da61d7f964e0fbbf7875a0095c560343cfb
KrishnaVamsiGoli1998 <47118943+KrishnaVamsiGoli1998@users.noreply.github.com>
2025-05-04 13:28:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const db = new Pool({
   password: process.env.DB_PASSWORD,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  database: "taskdb",
+  database: process.env.DB_NAME,
 });
 // Test route
 app.get("/", (req, res) => {
